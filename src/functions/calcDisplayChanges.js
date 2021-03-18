2e964c4bfd52b8b996c05b170666d95ce72686f8
Sebosun <sebos@protonmail.com>
2021-03-18 22:05:35 +0100

    some finishing touches lint was screaming bout

--- src/functions/calcDisplayChanges.js
+++ src/functions/calcDisplayChanges.js
@@ -2,7 +2,7 @@ function calcDisplayChanges(orgScreen, curScreen, charPosition) {
   // as in checkWaldo, first pos is Y second is X
   // here we check if there are any differences between pic size
   // and the current pic size
-  if (orgScreen[0] != curScreen[0] || orgScreen[1] != curScreen[1]) {
+  if (orgScreen[0] !== curScreen[0] || orgScreen[1] !== curScreen[1]) {
     let diff_Y = curScreen[0] / orgScreen[0];
     let diff_X = curScreen[1] / orgScreen[1];
     return [charPosition[0] * diff_Y, charPosition[1] * diff_X];
